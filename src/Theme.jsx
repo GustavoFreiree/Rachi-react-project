88fcc5c402c1319e8e9080d3f5d6fb14f55e7d96
GustavoFreire <86376279+GustavoFreiree@users.noreply.github.com>
2023-03-03 20:56:30 -0300

    update theme

---
+++ src/Theme.jsx
@@ -0,0 +1,12 @@
+const Theme = {
+	colors: {
+		white: "#fff",
+		white200: "#eff8ff",
+		white400: "#eff8ff",
+		gray: "#6e7275",
+		gray200: "#293145",
+		grayLight: "#dbdbdb",
+	},
+};
+
+export default Theme;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Logo = styled.img``;
+
+const Header = () => {
+	return (
+		<header>
+			<Logo src="../../assets/logo.svg" />
+		</header>
+	);
+};
